9244b3c56ddc2ca5a7b57880c7b6bd552b3f0681
jshenfield <jbshenfield@gmail.com>
2025-10-27 13:40:25 -0600

    added headers to each file

--- Item.java
+++ Item.java
@@ -1,5 +1,11 @@
 /* ENSF 614 - Lab 5 Exercise A and B
+ * File Name: Item.java
  * M. Moussavi, October 2024
+ * Lab Section: B01
+ * Completed by: Jack Shenfield & William Watson
+ * Submission Date: Oct 29, 2025
+ */
+
  * 
  */
 
