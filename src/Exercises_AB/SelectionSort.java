01b33725f044eacb57edd729563a5f1f1dff823d
jshenfield <jbshenfield@gmail.com>
2025-10-27 19:20:03 -0600

    Updated word file

--- src/Exercises_AB/SelectionSort.java
+++ src/Exercises_AB/SelectionSort.java
@@ -1,3 +1,11 @@
+/* ENSF 614 - Lab 5 Exercise A and B
+ * File Name: SelectionSort.java
+ * M. Moussavi, October 2024
+ * Lab Section: B01
+ * Completed by: Jack Shenfield & William Watson
+ * Submission Date: Oct 29, 2025
+ */
+
 package src.Exercises_AB;
 
 import java.util.ArrayList;
